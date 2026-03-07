71540d1aa615ee68e5322d70bf7fb7ec9a6bd0cf
Niv Nifusi <nivnin@gmail.com>
2026-03-07 15:47:32 +0000

    File changes

--- src/pages/ShiftChat.jsx
+++ src/pages/ShiftChat.jsx
@@ -69,7 +69,6 @@ export default function ShiftChat() {
         );
         setMessages(msgs);
         setHasNewMessages(false);
-        if (onUnreadChange) onUnreadChange(false);
         setLoading(false);
     };
 
