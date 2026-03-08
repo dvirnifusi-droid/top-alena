685cce8626ccf16f89512e4e75878d27202b4088
Niv Nifusi <nivnin@gmail.com>
2026-03-08 05:28:52 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -68,6 +68,9 @@ const adminLinks = [
   { title: "תבניות הודעה", url: createPageUrl("MessageTemplates"), icon: FileText, isSubItem: true },
   { title: "סקרי לקוחות", url: createPageUrl("CustomerSurveys"), icon: MessageSquare, isSubItem: true },
   { title: "ברקודי סקרים", url: createPageUrl("SurveyQRCodes"), icon: QrCode, isSubItem: true },
+
+  // חנות בגדים
+  { title: "ניהול חנות בגדים", url: createPageUrl("ApparelManagement"), icon: Trophy },
 ];
 
 // תפריט לעובדים רגילים - מחולק לקטגוריות
