a42cd57184daabd98884ad6f336e4e887426a393
Niv Nifusi <nivnin@gmail.com>
2026-03-08 17:48:37 +0000

    File changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -23,6 +23,7 @@ import InventoryScanner from '../components/dashboard/InventoryScanner';
 import SurveyQRGenerator from '../components/dashboard/SurveyQRGenerator';
 import ActiveEmployeesWidget from '../components/dashboard/ActiveEmployeesWidget';
 import BriefReadersWidget from '../components/dashboard/BriefReadersWidget';
+import DashboardViewersWidget from '../components/dashboard/DashboardViewersWidget';
 
 // רכיב סטטיסטיקות מהירות מעודכן
 function QuickStats() {
