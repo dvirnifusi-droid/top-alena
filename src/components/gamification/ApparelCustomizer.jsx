c3bb021be3e60338d4ae41289e0426ea4734e3a0
Niv Nifusi <nivnin@gmail.com>
2026-03-08 05:26:00 +0000

    File changes

--- src/components/gamification/ApparelCustomizer.jsx
+++ src/components/gamification/ApparelCustomizer.jsx
@@ -61,6 +61,7 @@ export default function ApparelCustomizer({ employeeId, employeeAvatar, onAvatar
   };
 
   useEffect(() => {
+    setOriginalAvatarUrl(employeeAvatar);
     setCurrentAvatarUrl(employeeAvatar);
   }, [employeeAvatar]);
 
