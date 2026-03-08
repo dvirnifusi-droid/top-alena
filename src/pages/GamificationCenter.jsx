d09ec940ecf5c56f0f3f4d219c35249332fe15b8
Niv Nifusi <nivnin@gmail.com>
2026-03-08 02:14:32 +0000

    File changes

--- src/pages/GamificationCenter.jsx
+++ src/pages/GamificationCenter.jsx
@@ -9,6 +9,7 @@ import ConfettiEffect from '../components/gamification/ConfettiEffect';
 import ShoutOutFeed from '../components/gamification/ShoutOutFeed';
 import DailyChallengeCard from '../components/gamification/DailyChallengeCard';
 import BadgesDisplay, { computeBadges } from '../components/gamification/BadgesDisplay';
+import AvatarUploader from '../components/gamification/AvatarUploader';
 import { Sun, Moon, Palette } from 'lucide-react';
 
 const RANKS = [
