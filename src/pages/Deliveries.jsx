3faae33193fc2171823de53b4566d7ea6158e90e
Niv Nifusi <nivnin@gmail.com>
2026-03-10 02:17:03 +0000

    File changes

--- src/pages/Deliveries.jsx
+++ src/pages/Deliveries.jsx
@@ -12,6 +12,13 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
   Camera,
   Package,
   CheckCircle2,
