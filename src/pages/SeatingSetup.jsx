b3560b442bc7011ba0618f8abb70b8728ded579a
Niv Nifusi <nivnin@gmail.com>
2026-04-10 12:21:13 +0000

    File changes

--- src/pages/SeatingSetup.jsx
+++ src/pages/SeatingSetup.jsx
@@ -498,7 +498,6 @@ export default function SeatingSetup() {
             alert("שגיאה בשמירת המפה.");
         } finally {
             setIsSaving(false);
-            loadLayout();
         }
     };
 
